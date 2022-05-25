4d75551d4a8e02b425f88426f0fcc18d14e402ca
Neapol94 <neapol.blamer@gmail.com>
2022-05-25 14:49:44 +0200

    refactoring style changes

--- src/app/pages/home/career/career.component.ts
+++ src/app/pages/home/career/career.component.ts
@@ -28,7 +28,7 @@ export class CareerComponent implements OnInit {
     {
       companyName: 'Humansoft sp. z o.o.',
       duration: this.portfolioService.returnYearsOrMonths(new Date('2018-11-14'), new Date()),
-      appointment: 'Junior Webdeveloper',
+      appointment: 'WebDeveloper/IT Technician',
       description: "I was developing CodeIgniter MVC-based portal using PHP, Javascript, jQuery, CSS 3 and HTML 5. I was getting data from database via MySQL in model, preparing it in controller and displaying in view. The portal's main purpose was to give user access to view and update their 'human resources and payroll' data - depending on user's role."
     }
   ]
